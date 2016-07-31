64801631e82ed18590bb0b1b85721ca0889208f2
XiaoChenYung <18730222889@126.com>
2016-07-31 13:19:10 +0800

    泛形

--- Swift闭包.playground/Contents.swift
+++ Swift闭包.playground/Contents.swift
@@ -11,4 +11,17 @@ func sun(num1 x:Int,num2 y:Int) -> Int {
 
 let f = sun
 
-print(<#T##items: Any...##Any#>) f(num1: 20, num2: 30)
+//print(<#T##items: Any...##Any#>) f(num1: 20, num2: 30)
+
+func swapTwoValue<T> (first a: T, sec b: T){
+    
+}
+
+var a = 10
+var b = 20
+
+//swap(&a, &b)
+swapTwoValue(first: a, sec: b)
+
+
+
